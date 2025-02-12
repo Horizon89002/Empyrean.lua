abea3854b54438ea982668e7238d2f73c402dfe8
randomguyontheinternet <arvinhizomi1999@gmail.com>
2025-02-12 22:29:31 +0400

    Update manage2.lua

--- manage2.lua
+++ manage2.lua
@@ -1,6 +1,6 @@
 local httpService = game:GetService('HttpService')
 local ThemeManager = {} do
-	ThemeManager.Folder = 'SaturnConfig'
+	ThemeManager.Folder = 'EmpyreanConfig'
 	-- if not isfolder(ThemeManager.Folder) then makefolder(ThemeManager.Folder) end
 
 	ThemeManager.Library = nil
