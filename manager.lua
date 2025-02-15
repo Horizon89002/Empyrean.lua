485814af4dedf645db83db5004be76ab12a5fbc8
randomguyontheinternet <arvinhizomi1999@gmail.com>
2025-02-15 21:03:07 +0400

    Update manager.lua

--- manager.lua
+++ manager.lua
@@ -1,7 +1,7 @@
 local httpService = game:GetService('HttpService')
 
 local SaveManager = {} do
-	SaveManager.Folder = 'LinoriaLibSettings'
+	SaveManager.Folder = 'EmpyreanConfig'
 	SaveManager.Ignore = {}
 	SaveManager.Parser = {
 		Toggle = {
