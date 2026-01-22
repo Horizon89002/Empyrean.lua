56644fc371f3d0f8195e0001647399c1f9b160e9
randomguyontheinternet <arvinhizomi1999@gmail.com>
2026-01-22 22:54:25 +0400

    Delete EmpyreanLoader.lua

--- EmpyreanLoader.lua
+++
@@ -1,9 +0,0 @@
-local gameId = game.PlaceId
-
-if gameId == 2788229376 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Horizon89002/Empyrean.lua/refs/heads/main/Empyreandh.lua"))()
-elseif gameId == 9825515356 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Horizon89002/Empyrean.lua/refs/heads/main/EmpyreanHC.lua"))()
-else
-    print("game not supported")
-end
